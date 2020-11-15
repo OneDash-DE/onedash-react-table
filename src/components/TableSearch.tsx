aaeefd46844cc5c11085d08d7fe95d15528b4363
Jonathan Bußhoff <jonathan@busshoff.net>
2020-11-15 17:01:16 +0100

    fix: prop removed

--- src/components/TableSearch.tsx
+++ src/components/TableSearch.tsx
@@ -2,7 +2,6 @@ import React from "react";
 
 interface TableSearchProps {
 	_onChange?: (value: string) => void;
-	onChange?: (value: string) => void;
 }
 const TableSearch = (props: TableSearchProps) => {
 	let timeout: NodeJS.Timeout;
@@ -10,7 +9,6 @@ const TableSearch = (props: TableSearchProps) => {
 
 	const sendData = () => {
 		props._onChange?.(value);
-		props.onChange?.(value);
 	};
 	const onChange = (e: any) => {
 		value = e.target.value;
