c74b537d58232f81edadaf70ef80ed52cbc0d4dc
Jonathan Bußhoff <jonathan@busshoff.net>
2020-12-02 13:45:56 +0100

    feat: added types to export

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import Table from "./components/Table";
 import Column from "./components/Column";
 import Cell from "./components/Cell";
 import Row from "./components/Row";
+import * as types from "./types";
 
-export { Table, Column, Cell, Row };
+export { Table, Column, Cell, Row, types };
